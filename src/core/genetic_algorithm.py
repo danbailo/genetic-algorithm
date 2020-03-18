b40c383e4fec2b55ec284d998e8b6e302317dddd
danbailo <danbailoufms@gmail.com>
2020-03-18 11:47:29 -0400

    n sei se esta funcionando corretamente, vou mudar a logica pra tirar o try

--- src/core/genetic_algorithm.py
+++ src/core/genetic_algorithm.py
@@ -1,34 +1,88 @@
-import numpy as np
+from itertools import combinations
 import random
 
-random.seed(1)
+# random.seed(1)
 
 class Genetic_Algorithm:
-	def __init__(self, n_individuals, epochs):
+	def __init__(self, n_individuals, len_individual):
 		self.n_individuals = n_individuals
-		self.epochs = epochs
+		self.len_individual = len_individual
 
 	def initialize_set(self, len_individual):
 		individuals = {}
 		for i in range(self.n_individuals):
 			individual = []
-			fitness = 0
 			for _ in range(len_individual):
-				gene = random.randint(0,1)
-				individual.append(gene)
-				if gene == 1: fitness += 1
-			individuals[i] = (individual, fitness)
+				individual.append(random.randint(0,1))
+			individuals[i] = [individual]
 		return individuals
 
-	def roulette_method(self, individual, individuals):
-		summation = sum(individuals.values())
-		return (individuals[individual]/summation)
+	def get_fitness(self, individuals):		
+		for individual, chromosome in individuals.items():
+			fitness = 0
+			for gene in chromosome[0]:
+				if gene == 1: fitness += 1
+			try:
+				individuals[individual][1] = fitness
+			except IndexError: #first iteration
+				individuals[individual].append(fitness)
+		return individuals
 
-	def linear_ranking_method(self, beta, individuals):
-		ranking = np.zeros(len(individuals))
+	def roulette_method(self, individuals):
+		summation = sum(list(map(lambda gene: gene[1], list(individuals.values()))))
+		for individual in individuals:
+			chance = individuals[individual][1]/summation
+			try:
+				individuals[individual][2] = chance
+			except IndexError: #first iteration
+				individuals[individual].append(chance)
+
+		new_individuals = {}
+		temp = []
 		i = 0
-		n = len(individuals)
-		for individual, value in individuals.items():
-			rank = beta-2 * (beta-1) * ((i-1) / n-1)
-			ranking[i] = (1/n) * rank
+		while len(new_individuals) != len(individuals):			
+			for individual, chromosome in individuals.items():
+				if chromosome[2] >= random.random():
+					new_individuals[i] = [chromosome[0]]
+					temp.append(chromosome[0])
 			i += 1
+
+		return individuals
+
+	def recombination(self, individuals, rate_recombination):
+		count_combination = 0
+		did_combination = False
+		for combination in list(combinations(list(individuals.items()),2)):
+			if rate_recombination >= random.random() and count_combination < 2: #crossover		
+				c_a, c_b = combination
+				index_c_a = c_a[0]
+				index_c_b = c_b[0]
+				slice_point = random.randint(0, len(c_a[1][0]))
+				new_c_a = [c_a[1][0][:slice_point] + c_b[1][0][slice_point:]]
+				new_c_b = [c_b[1][0][:slice_point] + c_a[1][0][slice_point:]]
+				count_combination += 1
+				did_combination = True
+
+			if did_combination:
+				individuals[index_c_a] = new_c_a
+				individuals[index_c_b] = new_c_b
+				did_combination = False
+
+		return individuals
+
+	def mutation(self, individuals, rate_mutation):		
+		did_mutation = False
+		for individual, chromosome in individuals.items():			
+			for i in range(len(chromosome[0])):				
+				if rate_mutation >= random.random():
+					if chromosome[0][i] == 0:
+						gene = 1
+					else:
+						gene = 0
+					new_chromosome = chromosome.copy()
+					new_chromosome[0][i] = gene
+					did_mutation = True
+			if did_mutation:
+				individuals[individual] = new_chromosome
+				did_mutation = False
+		return individuals
--- src/main.py
+++ src/main.py
@@ -2,14 +2,35 @@ from core import Genetic_Algorithm
 
 if __name__ == "__main__":
 	n_individuals = 10
-	epochs = 100
+	len_individual = 10
+	epochs = 1000
+	rate_recombination = 0.5
+	rate_mutation = 0.01
 
-	genetic_algorithm = Genetic_Algorithm(n_individuals, epochs)
+	genetic_algorithm = Genetic_Algorithm(n_individuals, len_individual)
 
-	individuals = genetic_algorithm.initialize_set(10)
+	print("First set")
+	individuals = genetic_algorithm.initialize_set(len_individual)
+	first = genetic_algorithm.get_fitness(individuals)
+	for individual, chromosome in first.items():
+		print(f"{chromosome}")
+	first = list(first.values())
+	summation = sum(list(map(lambda gene: gene[1], first)))
+	print(summation)
 
-	print(*individuals.items(), sep="\n")
 
-	print(list(individuals.values())[0][1])
+	for epoch in range(epochs):
+		# print(f"\nEpoch {epoch+1}")
+		individuals = genetic_algorithm.get_fitness(individuals)
+		individuals = genetic_algorithm.roulette_method(individuals)
+		individuals = genetic_algorithm.recombination(individuals, rate_recombination)
+		individuals = genetic_algorithm.mutation(individuals, rate_mutation)
+	print()
 
-	print(sum(list(map(lambda gene: gene[1], list(individuals.values())))))
+	print("Last set")
+	last = genetic_algorithm.get_fitness(individuals)
+	for individual, chromosome in last.items():
+		print(f"{chromosome[:2]}")
+	last = list(last.values())
+	summation = sum(list(map(lambda gene: gene[1], last)))
+	print(summation)		
